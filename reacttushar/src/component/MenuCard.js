115958ebd47f847d9bf77a4448443ce3a75892bb
TUSHAR MAURYA <tusharmaurya002@gmail.com>
2022-11-21 16:41:28 +0530

    ++

---
+++ reacttushar/src/component/MenuCard.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const MenuCard = ({menuData}) => {
+  return (
+    <>
+        <section className="main-card--cointainer">
+        {menuData.map((curElem)=>{
+            
+            return(
+                <>
+                <div className='card-container'>
+                    <div className='card'>
+                        <div className='card-body'>
+                            <span className='card-number card-circle subtle'>1</span>
+                            <span className='card-author subtle'>Breakfast</span>
+                            <h2 className='card-title'>Maggi</h2>
+                            
+                            <span className='card-description subtle'>The Maggi noodles is an instant food that has 
+                            gained popularity in several countries over the years. This comes in different flavors, 
+                            and people use it as a snack or light meal. The short captions for a Maggi post maybe 
+                            be about noodles, food, or even about being healthy. Here is a collection of captions 
+                            that you can use with your Maggi pictures.</span>
+                            <div className='card-read'>Read</div>
+
+                        </div>
+                        <img src=".\image\maggi.png" alt="images" className='card-media'></img>
+                        <span className='card-tag subtle'>Order Now</span>
+                    </div>
+                </div>
+                </>
+            );
+        })}  
+        </section>              
+    </>
+  );           
+};
+
+export default MenuCard
